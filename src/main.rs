a1f962f57fad4c53e9bfc7ba81ce66a1378ee6db
Kneelawk <kneelawk@gmail.com>
2023-01-29 17:44:28 -0800

    Use logger

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,22 @@
-use std::time::Duration;
+#[macro_use]
+extern crate log;
+
 #[allow(unused)]
-use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
+use esp_idf_sys as _;
+use std::time::Duration;
 
 fn main() {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
     esp_idf_sys::link_patches();
 
-    println!("Hello, world!");
+    esp_idf_svc::log::EspLogger::initialize_default();
+
+    info!("Hello, world!");
 
     // This crashes on Xtensa processors for some reason :/
     // Good thing this is a RISC-V processor :)
     std::thread::sleep(Duration::from_millis(1000));
 
-    println!("Slept for 1 second!");
+    info!("Slept for 1 second!");
 }
